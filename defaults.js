e244fe56b971df63d37d3d1f70bde448584be82b
Abdul Razak <moabdulrazak17@gmail.com>
2025-01-11 13:17:40 +0000

    chore: add defaults.js

---
+++ defaults.js
@@ -0,0 +1,240 @@
+/*******************************************************************************
+   Surfing Default Mappings
+ ******************************************************************************/
+
+/*
+Help
+<Alt-s>Toggle SurfingKeys on current site
+<Alt-i>Enter PassThrough mode to temporarily suppress SurfingKeys
+pEnter ephemeral PassThrough mode to temporarily suppress SurfingKeys
+?Show usage
+;qlShow last action
+.Repeat last action
+Mouse Click
+cfOpen multiple links in a new tab
+giGo to the first edit box
+gfOpen a link in non-active new tab
+[[Click on the previous link on current page
+]]Click on the next link on current page
+;mmouse out last element
+;fsDisplay hints to focus scrollable elements
+;diDownload image
+iGo to edit box
+IGo to edit box with vim editor
+OOpen detected links from text
+fOpen a link, press SHIFT to flip overlapped hints, hold SPACE to hide hints
+afOpen a link in active new tab
+COpen a link in non-active new tab
+<Ctrl-h>Mouse over elements.
+<Ctrl-j>Mouse out elements.
+<Ctrl-i>Go to edit box with vim editor
+qClick on an Image or a button
+Scroll Page / Element
+0Scroll all the way to the left
+cSReset scroll target
+csChange scroll target
+eScroll half page up
+UScroll full page up
+dScroll half page down
+PScroll full page down
+ggScroll to the top of the page
+GScroll to the bottom of the page
+jScroll down
+kScroll up
+hScroll left
+lScroll right
+$Scroll all the way to the right
+%Scroll to percentage of current page
+;wFocus top window
+wSwitch frames
+uScroll half page up
+Tabs
+ytDuplicate current tab
+yTDuplicate current tab in background
+g0Go to the first tab
+g$Go to the last tab
+gx0Close all tabs on left
+gxtClose tab on left
+gxTClose tab on right
+gx$Close all tabs on right
+gxxClose all tabs except current one
+gxpClose playing tab
+EGo one tab left
+RGo one tab right
+TChoose a tab
+;gtGather filtered tabs into current window
+;gwGather all tabs into current window
+zrzoom reset
+zizoom in
+zozoom out
+<Alt-p>pin/unpin current tab
+<Alt-m>mute/unmute current tab
+onOpen newtab
+xClose current tab
+XRestore closed tab
+WMove current tab to another window
+<<Move current tab to left
+>>Move current tab to right
+Page Navigation
+guGo up one path in the URL
+gTGo to first activated tab
+gtGo to last activated tab
+gpGo to the playing tab
+g?Reload current page without query string(all parts after question mark)
+g#Reload current page without hash fragment
+gUGo to root of current URL hierarchy
+;uEdit current URL with vim editor, and open in new tab
+;UEdit current URL with vim editor, and reload
+BGo one tab history back
+FGo one tab history forward
+<Ctrl-6>Go to last used tab
+SGo back in history
+DGo forward in history
+rReload the page
+Sessions
+ZZSave session and quit
+ZRRestore last session
+Search selected with
+sgSearch selected with google
+sdSearch selected with duckduckgo
+sbSearch selected with baidu
+seSearch selected with wikipedia
+swSearch selected with bing
+ssSearch selected with stackoverflow
+shSearch selected with github
+sySearch selected with youtube
+Clipboard
+yGCapture current full page
+ySCapture scrolling element
+yvYank text of an element
+ymvYank text of multiple elements
+ymaCopy multiple link URLs to the clipboard
+ymcCopy multiple columns of a table
+ygCapture current page
+yaCopy a link URL to the clipboard
+ycCopy a column of a table
+yqCopy pre text
+yiYank text of an input
+ysCopy current page's source
+yjCopy current settings
+yyCopy current page's URL
+yYCopy all tabs's url
+yhCopy current page's host
+ylCopy current page's title
+yQCopy all query history of OmniQuery.
+yfCopy form data in JSON on current page
+ypCopy form data for POST on current page
+ydCopy current downloading URL
+cqQuery word with Hints
+ccOpen selected link or link from clipboard
+;ppPaste html on current page
+;pjRestore settings data from clipboard
+;pfFill form with data from yf
+;cqClear all URLs in queue to be opened
+Omnibar
+goOpen a URL in current tab
+QOpen omnibar for word translation
+abBookmark current page to selected folder
+oiOpen incognito window
+omOpen URL from vim-like marks
+ogOpen Omnibar for google Search
+odOpen Omnibar for duckduckgo Search
+obOpen Omnibar for baidu Search
+oeOpen Omnibar for wikipedia Search
+owOpen Omnibar for bing Search
+osOpen Omnibar for stackoverflow Search
+oyOpen Omnibar for youtube Search
+oxOpen recently closed URL
+ohOpen URL from history
+HOpen opened URL in current tab
+:Open commands
+tOpen a URL
+bOpen a bookmark
+<Ctrl-d>Delete focused item from bookmark or history
+<Ctrl-i>Edit selected URL with vim editor, then open
+<Ctrl-j>Toggle Omnibar's position
+<Ctrl-.>Show results of next page
+<Ctrl-,>Show results of previous page
+<Ctrl-c>Copy selected item url or all listed item urls
+<Ctrl-D>Delete all listed items from bookmark or history
+<Ctrl-r>Re-sort history by visitCount or lastVisitTime
+<Esc>Close Omnibar
+<Ctrl-m>Create vim-like mark for selected item
+<Tab>Forward cycle through the candidates.
+<Shift-Tab>Backward cycle through the candidates.
+<Ctrl-'>Toggle quotes in an input element
+<ArrowDown>Forward cycle through the candidates.
+<ArrowUp>Backward cycle through the candidates.
+<Ctrl-n>Forward cycle through the candidates.
+<Ctrl-p>Backward cycle through the candidates.
+Visual Mode
+/Find in current page
+zvEnter visual mode, and select whole element
+VRestore visual mode
+*Find selected text in current page
+vToggle visual mode
+nNext found text
+NPrevious found text
+0backward lineboundary
+lforward character
+hbackward character
+jforward line
+kbackward line
+wforward word
+eforward word
+bbackward word
+)forward sentence
+(backward sentence
+}forward paragraphboundary
+{backward paragraphboundary
+$forward lineboundary
+Gforward documentboundary
+ggbackward documentboundary
+oGo to Other end of highlighted text
+*Search word under the cursor
+<Enter>Click on node under cursor.
+<Shift-Enter>Click on node under cursor.
+ztmake cursor at top of window.
+zzmake cursor at center of window.
+zbmake cursor at bottom of window.
+fForward to next char.
+FBackward to next char.
+;Repeat latest f, F
+,Repeat latest f, F in opposite direction
+pExpand selection to parent element
+VSelect a word(w) or line(l) or sentence(s) or paragraph(p)
+<Ctrl-u>Backward 20 lines
+<Ctrl-d>Forward 20 lines
+tTranslate selected text with google
+qTranslate word under cursor
+vim-like marks
+mAdd current URL to vim-like marks
+'Jump to vim-like mark
+<Ctrl-'>Jump to vim-like mark in new tab.
+Settings
+;pmPreview markdown
+;eEdit Settings
+Chrome URLs
+gsView page source
+;jClose Downloads Shelf
+Misc
+;phPut histories from clipboard
+;tTranslate selected text with google
+;dhDelete history older than 30 days
+;dbRemove bookmark for current page
+;yhYank histories
+Insert Mode
+<Ctrl-e>Move the cursor to the end of the line
+<Ctrl-f>Move the cursor to the beginning of the line
+<Ctrl-u>Delete all entered characters before the cursor
+<Alt-b>Move the cursor Backward 1 word
+<Alt-f>Move the cursor Forward 1 word
+<Alt-w>Delete a word backwards
+<Alt-d>Delete a word forwards
+<Esc>Exit insert mode
+<Ctrl-'>Toggle quotes in an input element
+<Ctrl-i>Open vim editor for current input
+
+More help
+ */
+
